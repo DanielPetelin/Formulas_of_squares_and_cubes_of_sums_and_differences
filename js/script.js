66ed34b89509cf008f13267da9654e39894faeb4
Daniil Petelin <DaniilPetelin77@gmail.com>
2020-02-19 17:51:59 +0300

    add third formul

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Формула квадрата суммы
 document.getElementById('for').onclick = function() {
     var num1 = document.getElementById('num1').value;
     var num2 = document.getElementById('num2').value;
@@ -6,10 +7,20 @@ document.getElementById('for').onclick = function() {
     document.getElementById('out').innerHTML = result;
 };
 
+// Формула квадрата разности
 document.getElementById('for2').onclick = function() {
     var num3 = document.getElementById('num3').value;
     var num4 = document.getElementById('num4').value;
     var result = Math.pow(num3, 2) - (2 * num3 * num4) + Math.pow(num4, 2);
 
     document.getElementById('out2').innerHTML = result;
+};
+
+// Формула куба суммы
+document.getElementById('for3').onclick = function() {
+    var num5 = document.getElementById('num5').value;
+    var num6 = document.getElementById('num6').value;
+    var result = Math.pow(num5, 3) + (3 * Math.pow(num5, 2) * num6) + (3 * Math.pow(num6, 2) * num5) + Math.pow(num6, 3);
+
+    document.getElementById('out3').innerHTML = result;
 };
